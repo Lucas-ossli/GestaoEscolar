cbae7752dafa70ecaafdc79b4b4cd6a3f636896b
Lucas-ossli <90059655+Lucas-ossli@user.noreply.github.com>
2023-04-20 18:12:24 -0300

    Criação .Net MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GestaoEscolar.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
