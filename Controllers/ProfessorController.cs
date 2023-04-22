94f10f063402bcc04dbdf29e592adb4a1a4e625c
Lucas <90059655+Lucas-ossli@users.noreply.github.com>
2023-04-22 12:42:48 -0300

    Criando select de turmas

--- Controllers/ProfessorController.cs
+++ Controllers/ProfessorController.cs
@@ -6,48 +6,41 @@ namespace GestaoEscolar.Controllers;
 
 public class ProfessorController : Controller
 {
-    
-    public IActionResult Index(int id)
+    [HttpGet]
+    //TODO - Adicionar o Id do professor que vira no Identity
+    public IActionResult Index(/*int id*/)
     {       
         
         var turmas = new List<Turma>();
-
         turmas.Add(new Turma(){
-            Nome = "1º A",
-            QtdAlunos = 5,
-            CdTurma = 1
-            
+            NomeDaDisciplina = "Banco de dados",
+            NomeDaTurma = "1-a",
+            QtdAlunos = 20,
+            CdTurma = 1,
+            CdDisciplina = 1
         });
 
-         turmas.Add(new Turma(){
-            Nome = "1º B",
-            QtdAlunos = 4,
-            CdTurma = 2
-        });
+        //var turmas = new Turma().Search(1);
 
         return View(turmas);
     }
-
-    [HttpGet]
-    public IActionResult Chamada(int id)
+    
+    [Route("Professor/Chamada/{cdturma}/{cddisciplina}")]
+    public IActionResult Chamada(int cdTurma, int cddisciplina)
     {
         var lista = new List<Chamada>();
 
-        if(id == 1){
-            lista.Add(new Chamada("Pedro"));
-            lista.Add(new Chamada("Christian"));
-            lista.Add(new Chamada("Jean"));
-            lista.Add(new Chamada("Larissa"));
-            lista.Add(new Chamada("João"));
-            
-        }
-        else if(id == 2)
-        {
-            lista.Add(new Chamada("Leonardo"));
-            lista.Add(new Chamada("Lucas"));
-            lista.Add(new Chamada("Karen"));
-            lista.Add(new Chamada("Christian H."));
-        }
+        lista.Add(new Chamada("Pedro"));
+        lista.Add(new Chamada("Christian"));
+        lista.Add(new Chamada("Jean"));
+        lista.Add(new Chamada("Larissa"));
+        lista.Add(new Chamada("João"));
+    
+        lista.Add(new Chamada("Leonardo"));
+        lista.Add(new Chamada("Lucas"));
+        lista.Add(new Chamada("Karen"));
+        lista.Add(new Chamada("Christian H."));
+    
         
         //var retorno = chamada.Search();
         
--- Models/Turma.cs
+++ Models/Turma.cs
@@ -1,37 +1,87 @@
 public class Turma
 {
-    public int CdTurma { get; set; }
-    public string? Nome { get; set; }
+
+    public string? NomeDaTurma { get; set; }
+    public string? NomeDaDisciplina { get; set; } //Engenharia
     public int QtdAlunos { get; set; }
+    public int CdTurma { get; set; }
+    public int CdDisciplina { get; set; }
     public int CdProfessor { get; set; }
 
 
     public List<Turma> Search(int cdProfessor)
     {
+         var turmas = new List<Turma>();
         //select do nome e numero da turma do professor 
-        /*
-        select
-        Pr.IdTurma,
-        Tu.Nome,
-        count(Al)
-        from Professor Pr 
-        where IdProfessor = @CdProfessor
-        inner join Turma Tu
-        on Tu.IdTurma = Pr.IdTurma
-        inner join Aluno Al
-        on Al.IdTurma = Pr.IdTurma
-        */
-        var turmas = new List<Turma>();
-
+        var sql = "select  "
+        +"di.nomeDisciplina, "
+        +"tu.Nome, "
+        +"tu.TurmaId, "
+        +"di.idDisciplina" 
+        + "from Disciplinas di, ProfessorTurma Pt "
+        + "inner join Turma tu on tu.TurmaId = Pt.TurmaId "
+        + "where di.professorId = @CdProfessor and pt.ProfessorId = @CdProfessor ";
         
-        foreach(var i in  turmas){
+        /*
+        using(var cn = new SqlConnection(_conn))
+        {
+            cn.Open();
+            using(var cmd = new SqlCommand(sql, cn))
+            {
+                using(var dr = cmd.ExecuteReader())
+                {
+                    if(dr.HasRows)
+                    {
+                        while(dr.Read())
+                        {
+                            turmas.Add(new Turma(){
+                                NomeDaDisciplina = dr["nomeDisciplina"].ToString(),
+                                NomeDaTurma = dr["Nome"].ToString(),
+                                CdTurma = Convert.ToInt32(dr["TurmaId"]),
+                                CdDisciplina = Convert.ToInt32(dr["idDisciplina"])
+                            });
+                        }
+                    }
+                }
+            }
+        }
+        */
+
+        var sql2Count = "select count(*) as QuantidadeAluno from Aproveitamentos Ap "
+        +"where Ap.TurmaId = @CdTurma and disciplinaId = @CdDisciplina";
+        List<int> qtdAlunos = new List<int>();
+        foreach(var item in turmas){
             /*
-            select count(*) from Aluno Al
-            where Al.IdTurma = @CdTurma
+            
+
+            using(var cmd EntityCommand(sql2Count, conn))
+            {
+                cmd.Parameters.Add(new EntityParameter(){
+                ParameterName = "@CdTurma",
+                value = item.CdTurma
+                });
+
+                cmd.Parameters.Add(new EntityParameter(){
+                    ParameterName = "@CdDisciplina",
+                    value = item.CdDisciplina
+                });
+
+                using (DbDataReader rdr = cmd.ExecuteReader(CommandBehavior.SequentialAccess))
+                {
+                    while(rdr.Read())
+                    {
+                        qtdAlunos.add(Convert.ToInt32(rdr["QuantidadeAluno"]));
+                    }
+                }
+            }
             */
         }
         
-        return null;
+        for(int i =0; i < turmas.Count ; i++){
+            turmas[i].QtdAlunos = qtdAlunos[i];
+        }
+
+        return turmas;
     }
 
 }
